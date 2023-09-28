7f4fe00605bf47cb67b41f0df12b299b2408dad4
Mike Creighton <mike@mikecreighton.com>
2023-09-27 17:18:02 -0700

    Changing the standalone version to be port 5000 to avoid conflicts.

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def generate():
     return jsonify(story_object)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5000)
