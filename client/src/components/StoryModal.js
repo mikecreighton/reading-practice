78e3635ecca764cf1f11a0a371a40659d7e7ff4a
Mike Creighton <mike@mikecreighton.com>
2024-07-25 20:22:34 -0700

    Improved comments for StoryModal component.

--- client/src/components/StoryModal.js
+++ client/src/components/StoryModal.js
@@ -2,14 +2,17 @@ import { React, useEffect } from "react";
 import { gsap, Power4 } from "gsap";
 import "./StoryModal.scss";
 
+// StoryModal component displays the generated story and handles modal actions
 function StoryModal({ story, onRegenerate, onCloseComplete, onCancelRequest }) {
     
   // This effect is called when the story modal is first rendered and kicks off the opening animation
+  // Effect to animate the modal when it is first rendered
   useEffect(() => {
     gsap.to(".modal", { duration: 0.5, y: "0%", ease: Power4.easeOut });
   }, []);
 
   // This function is called when the user clicks the "X" button in the story modal which kicks off closing animation
+  // Handle the close request for the modal
   const handleCloseRequest = (event) => {
     event.preventDefault();
     onCancelRequest();
