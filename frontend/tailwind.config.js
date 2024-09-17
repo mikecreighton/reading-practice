2a70d643dbea72bed088559758307fc28ec0ba33
Mike Creighton <mike@mikecreighton.com>
2024-09-17 16:39:34 -0700

    Added the bubblegum theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -49,7 +49,7 @@ export default {
             "button-option-text": colors.gray["800"],
             "button-option-border": colors.gray["50"],
 
-            "button-option-hover": colors.gray["50"],
+            "button-option-hover": colors.white,
             "button-option-hover-text": colors.gray["800"],
             "button-option-hover-border": colors.gray["300"],
 
@@ -72,40 +72,50 @@ export default {
           selectors: [".bubblegum", ".theme-bubblegum"],
           extend: {
             colors: {
-              primary: "#98FB98",
+              primary: colors.purple["950"],
               secondary: "#87CEEB",
               accent: "#FF69B4",
-              background: "#FFB6DA",
-              text: "#4B0082",
-              "bottom-bar": "#FF80BF",
+              background: colors.pink["200"],
+              text: colors.indigo["950"],
 
-              story: "#FFB6DA",
+              "bottom-bar": colors.pink["100"],
+              story: colors.pink["200"],
+              "story-illustration-border": colors.pink["300"],
 
-              "button-primary": "#98FB98",
-              "button-primary-text": "#215921",
-              "button-primary-border": "#8CF88C",
-              "button-primary-hover": "#ACFCAC",
+              chip: colors.pink["300"],
+              "chip-text": colors.indigo["950"],
 
-              "button-secondary": "#FFD6EA",
-              "button-secondary-text": "#971958",
-              "button-secondary-border": "#E285B4",
+              "button-primary": colors.purple["950"],
+              "button-primary-text": colors.pink["50"],
+              "button-primary-border": colors.purple["950"],
+              "button-primary-hover": colors.purple["900"],
 
-              "button-secondary-hover": "#B5E1F3",
-              "button-secondary-hover-text": "#0E6386",
-              "button-secondary-hover-border": "#3AA4CF",
+              "button-secondary": colors.pink["200"],
+              "button-secondary-text": colors.indigo["950"],
+              "button-secondary-border": colors.pink["200"],
 
-              "button-secondary-selected": "#B5E1F3", // This is for form element buttons
-              "button-secondary-selected-text": "#0E6386",
-              "button-secondary-selected-border": "#3AA4CF", // This is for form element buttons
+              "button-secondary-hover": colors.pink["300"],
+              "button-secondary-hover-text": colors.indigo["950"],
+              "button-secondary-hover-border": colors.pink["300"],
 
-              "input-border": "#E285B4",
-              "input-background": "#FFD6EA",
-              "input-border-focus": "#D33484",
-              "input-text": "#4B0082",
-              "input-placeholder": "#D275C6",
+              "button-option": colors.pink["100"],
+              "button-option-text": colors.indigo["950"],
+              "button-option-border": colors.pink["100"],
 
-              chip: "#B5E1F3",
-              "chip-text": "#0E6386",
+              "button-option-hover": colors.pink["50"],
+              "button-option-hover-text": colors.indigo["950"],
+              "button-option-hover-border": colors.pink["300"],
+
+              "button-option-selected": colors.pink["50"],
+              "button-option-selected-text": colors.indigo["950"],
+              "button-option-selected-border": colors.pink["400"],
+
+              "input-border": colors.pink["100"],
+              "input-background": colors.pink["100"],
+              "input-border-focus": colors.pink["400"],
+              "input-background-focus": colors.pink["50"],
+              "input-text": colors.indigo["950"],
+              "input-placeholder": colors.pink["300"],
             },
           },
         },
