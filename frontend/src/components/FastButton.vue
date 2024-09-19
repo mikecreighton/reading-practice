fad8e9754ee1b3f97a11510dc7f24886f5b9506a
Mike Creighton <mike@mikecreighton.com>
2024-09-18 17:36:37 -0700

    Fixed FastButton so that we check if touch is inside the button on touchend.

--- frontend/src/components/FastButton.vue
+++ frontend/src/components/FastButton.vue
@@ -59,23 +59,25 @@ const emit = defineEmits(["click"])
 const addListener = (el, type, listener, useCapture) => {
   el.addEventListener(type, listener, useCapture)
   return {
-    destroy: () => el.removeEventListener(type, listener, useCapture)
+    destroy: () => el.removeEventListener(type, listener, useCapture),
   }
 }
 
 const onTouchStart = (event) => {
   event.stopPropagation()
   touchEvents.value.push(
-    addListener(buttonRef.value, 'touchend', onClick, false),
-    addListener(document.body, 'touchmove', onTouchMove, false)
+    addListener(buttonRef.value, "touchend", onClick, false),
+    addListener(document.body, "touchmove", onTouchMove, false),
   )
   startX.value = event.touches[0].clientX
   startY.value = event.touches[0].clientY
 }
 
 const onTouchMove = (event) => {
-  if (Math.abs(event.touches[0].clientX - startX.value) > 10 ||
-      Math.abs(event.touches[0].clientY - startY.value) > 10) {
+  if (
+    Math.abs(event.touches[0].clientX - startX.value) > 10 ||
+    Math.abs(event.touches[0].clientY - startY.value) > 10
+  ) {
     reset()
   }
 }
@@ -84,15 +86,26 @@ const onClick = (event) => {
   event.stopPropagation()
   reset()
   if (!props.isDisabled) {
-    emit("click", event)
+    if (event.type === "touchend") {
+      // We need to make sure that the touch up event is inside of the button.
+      const x = event.changedTouches[0].clientX
+      const y = event.changedTouches[0].clientY
+      // Check if the x and y are within the button's bounds.
+      const rect = buttonRef.value.getBoundingClientRect()
+      if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+        emit("click", event)
+      }
+    } else {
+      emit("click", event)
+    }
   }
-  if (event.type === 'touchend') {
+  if (event.type === "touchend") {
     preventGhostClick(startX.value, startY.value)
   }
 }
 
 const reset = () => {
-  touchEvents.value.forEach(event => event.destroy())
+  touchEvents.value.forEach((event) => event.destroy())
   touchEvents.value = []
 }
 
@@ -118,13 +131,13 @@ const onDocumentClick = (event) => {
 
 onMounted(() => {
   if ("ontouchstart" in window) {
-    document.addEventListener('click', onDocumentClick, true)
+    document.addEventListener("click", onDocumentClick, true)
   }
 })
 
 onUnmounted(() => {
   if ("ontouchstart" in window) {
-    document.removeEventListener('click', onDocumentClick, true)
+    document.removeEventListener("click", onDocumentClick, true)
   }
 })
 </script>
