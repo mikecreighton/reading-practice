9a2100b839565a90acc62fb72f4c149f01b48bc9
Mike Creighton <mike@mikecreighton.com>
2024-09-30 11:48:06 -0700

    Added debug values in separate file.

---
+++ frontend/src/debug-values.js
@@ -0,0 +1,16 @@
+export const debugWordList = [
+  "park",
+  "puzzle",
+  "penguin",
+  "pasta",
+  "pizza",
+  "pepperoni",
+  "plaza",
+  "poppy",
+  "popsicle",
+  "poodle",
+]
+export const debugCharacterName = "A scientist"
+export const debugSetting = "A school bus"
+export const debugHumor = 10
+export const debugStoryContent = `The scientist boarded the school bus, carrying a giant puzzle of a penguin. As the bus bounced along, she tried to eat her pasta lunch, but the noodles kept flying everywhere! Suddenly, the bus hit a bump, and her puzzle pieces scattered all over. The scientist scrambled to pick them up, accidentally grabbing a slice of pizza from a student's lunchbox instead of a puzzle piece. She stuck the pizza slice onto her puzzle, creating a very strange penguin with a cheesy beak. The bus driver saw this in the mirror and couldn't stop giggling. He missed the turn for the school and ended up at a park. All the kids cheered, thinking they were getting a surprise field trip, while the confused scientist stood there holding her pizza-penguin puzzle.`
