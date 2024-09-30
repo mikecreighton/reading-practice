3552b74c7952fa6a5ab91da7c94f179afd4e2675
Mike Creighton <mike@mikecreighton.com>
2024-09-30 11:02:48 -0700

    Forgot to add the settings constants for local storage keys.

---
+++ frontend/src/settings-constants.js
@@ -0,0 +1,6 @@
+export const LOCAL_STORAGE_SETTINGS_KEY = "reading-practice-settings"
+export const LOCAL_STORAGE_INPUTS_KEY = "reading-practice-inputs"
+export const DEFAULT_SETTINGS = {
+  gradeLevel: "2nd",
+  theme: "default",
+}
